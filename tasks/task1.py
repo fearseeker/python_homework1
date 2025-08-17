c1e287fe36f30c8b21e50bb553564cb6c6772321
fearseeker <cah9ivin@gmail.com>
2025-08-17 19:41:23 +0000

    перше завдання

--- tasks/task1.py
+++ tasks/task1.py
@@ -1,1 +1,10 @@
-
+a = int(input("введить першу сторону: "))
+b = int(input("введить другу сторону: "))
+c = int(input("введить третю: "))
+nums = [a,b,c]
+def Istriangl(nums):
+    if (nums[0]*nums[0]) + (nums[1]*nums[1]) == nums[2]*nums[2] or (nums[1]*nums[1]) + (nums[2]*nums[2]) == nums[0]*nums[0] or (nums[2]*nums[2]) + (nums[1]*nums[1]) == nums[0]*nums[0]:
+        print("True")
+    else:
+        print("False")
+Istriangl(nums)
